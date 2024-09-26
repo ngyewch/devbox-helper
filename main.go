f63ebd3276554739918dd1996c611974bae43ef1
Nick Ng <ngyewch@gmail.com>
2024-09-26 10:54:56 +0800

    chore: release

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
+	version         string
+	commit          string
+	commitTimestamp string
+
 	projectDirFlag = &cli.PathFlag{
 		Name:    "project-dir",
 		Usage:   "project directory",
@@ -17,8 +25,9 @@ var (
 
 func main() {
 	app := &cli.App{
-		Name:  "devbox-helper",
-		Usage: "Devbox helper",
+		Name:    "devbox-helper",
+		Usage:   "Devbox helper",
+		Version: version,
 		Commands: []*cli.Command{
 			{
 				Name:  "project",
@@ -41,6 +50,30 @@ func main() {
 				Action:    doLatest,
 			},
 		},
+	}
+
+	cli.VersionPrinter = func(cCtx *cli.Context) {
+		var parts []string
+		if version != "" {
+			parts = append(parts, fmt.Sprintf("version=%s", version))
+		}
+		if commit != "" {
+			parts = append(parts, fmt.Sprintf("commit=%s", commit))
+		}
+		if commitTimestamp != "" {
+			formattedCommitTimestamp := func(commitTimestamp string) string {
+				epochSeconds, err := strconv.ParseInt(commitTimestamp, 10, 64)
+				if err != nil {
+					return ""
+				}
+				t := time.Unix(epochSeconds, 0)
+				return t.Format(time.RFC3339)
+			}(commitTimestamp)
+			if formattedCommitTimestamp != "" {
+				parts = append(parts, fmt.Sprintf("commitTimestamp=%s", formattedCommitTimestamp))
+			}
+		}
+		fmt.Println(strings.Join(parts, " "))
 	}
 
 	err := app.Run(os.Args)
